e695ec01fa6cbdfd1f21088e41f55f003d9c36d4
Diego-VT <torresvieiradiego@gmail.com>
2024-04-30 17:04:51 -0300

    Resposta do Desafio conta bancaria através do Terminal/console

---
+++ Desafio Banco Terminal/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Informe numero da conta bancaria: ");
+        int numeroConta = ler.nextInt();
+
+        ler.nextLine();
+
+        System.out.println("Informe Agencia: ");
+        String agencia =  ler.nextLine();
+
+        System.out.println("Informe o Nome do Cliente: ");
+        String nome = ler.nextLine();
+
+        System.out.println("Informe o saldo: ");
+        double saldo = ler.nextDouble();
+
+        System.out.println("Olá " + nome + ", Obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque");
+
+
+        ler.close();
+    }
+}
