4d06de6a9899cb69a3b054ab692686248bfbe93e
Diego-VT <torresvieiradiego@gmail.com>
2024-04-30 16:40:22 -0300

    Atualização de codigos

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -2,6 +2,7 @@ public class SmartTv {
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+    public String formatarCep(long cep){}
 
     public void mudarCanal(int novoCanal){
         canal = novoCanal;
